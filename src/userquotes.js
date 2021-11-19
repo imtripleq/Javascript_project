ecd943748a5def560b17fc4e0628fa335f7c93d5
imtripleq <flamework89@hotmail.com>
2021-11-19 21:26:20 +1100

    Resolving Put Request

--- src/userquotes.js
+++ src/userquotes.js
@@ -12,17 +12,37 @@ function fetchToMock(quote) {
 }
 
 ///////////// Add to list
-function fetchFromMock() {
-  return fetch("http://localhost:4000/users")
-    .then((resp) => resp.json())
-    .then((data) => {
-      console.log(data);
-      data.forEach((e) => {
-        const list = document.createElement("tr");
-        list.innerHTML = `<td>${data}<td>`;
-      });
-    });
-}
+// function fetchFromMock() {
+//   fetch("http://localhost:4000/users")
+//     .then((resp) => resp.json())
+//     .then((data) => {
+//       console.log(data);
+//       // data.forEach((e) => {
+//       //   const list = document.createElement("tr");
+//       //   list.innerHTML = `<td>${data}<td>`;
+//       // });
+//     });
+// }
 
-// fetchToMock("hahaha");
-// console.log();
+////// Add Quotes
+// const addQuotes = (ori, key, value) => {
+//   ori[key] = value;
+
+//   return ori;
+// };
+
+function updateToMock(quote) {
+  return fetch("http://localhost:4000/users", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(quote),
+  });
+}
+const mockUser = {
+  id: "abc",
+  array: ["test Quote 1"],
+};
+
+updateToMock(mockUser);
